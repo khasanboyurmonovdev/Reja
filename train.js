c6c592bff558b2537a5d175f83072a13273fcc1d
sarvarbekovich <khasanboyrumonov1998@yahoo.com>
2024-08-30 12:22:23 +0900

    feat: E-Task MIT . vazifa bajarildi

--- train.js
+++ train.js
@@ -1,9 +1,29 @@
+/*E-TASK: 
+
+Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+MASALAN: getReverse("hello") return qilsin "olleh"
+
+@MITASK
+*/
+
+function getReverse(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+// Misol
+console.log(getReverse("magistr"));
+
 /*
 D-TASK: 
 
 Shunday class tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
 MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 */
+/*
 class Shop {
   constructor(bolka, osh, fanta) {
     this.inventory = {
@@ -60,7 +80,7 @@ shop.qoldiq(); // Qoldiqni ko'rsatadi
 shop.sotish("bolka", 3); // 3ta non sotadi
 shop.qabul("fanta", 4); // 4ta somsa qabul qiladi
 shop.qoldiq(); // Qoldiqni ko'rsatadi
-
+*/
 //MIT TASK-C
 /*C-TASK: 
 
